0b2c6950431c20f2b79e6edaf2245069bb6c1fd2
Jorge Henriquez <joahenri@ucsc.edu>
2018-08-24 22:56:00 -0700

    Update stress to inlclue mc radius

--- stress.c
+++ stress.c
@@ -5,7 +5,7 @@
 #include "flags.h"
 
 #ifndef NUM_ARGS
-#define NUM_ARGS 4
+#define NUM_ARGS 5
 #endif
 
 #ifndef NUM_TESTS
@@ -17,19 +17,22 @@ typedef enum
 	QUIET,
 	VERBOSE,
 	NUM,
-	MONTECARLO
+	MONTECARLO,
+	MONTECARLO_RADIUS
 } argflag_t;
 
-void printFlags(flag_t *flag, size_t n);
+void printFlags(flag_t *flag, long long n, long long r );
 
 int main(int argc, char const *argv[])
 {
 
-	size_t iterations = 50;
+	long long iterations = 50;
+	long long monteRadius = 50;
+
 	flag_t *flags = newFlag(NUM_ARGS);
 	int option;
 
-	while ((option = getopt(argc, (char **)argv, "qvmn:")) != -1)
+	while ((option = getopt(argc, (char **)argv, "qvmn:r:")) != -1)
 	{
 		switch (option)
 		{
@@ -49,26 +52,34 @@ int main(int argc, char const *argv[])
 		case 'm':
 			setFlag(flags, MONTECARLO, true);
 			break;
+
+		case 'r':
+			setFlag(flags,MONTECARLO_RADIUS, true);
+			monteRadius = atoi(optarg);
+			break;
 		}
 	}
 
 	if (checkFlag(flags, VERBOSE))
 	{
-		printFlags(flags, iterations);
+		printFlags(flags, iterations, monteRadius);
 	}
 
 	delFlag(&flags);
 	return 0;
 }
 
-void printFlags(flag_t *flag, size_t n)
+void printFlags(flag_t *flag, long long n, long long r )
 {
 	printf("VERBOSE: Flag \"VERBOSE\" is on\n");
 	printf("VERBOSE: Flag \"QUIET\" is ");
 	checkFlag(flag, QUIET) == true ? printf("on\n") : printf("off\n");
 	printf("VERBOSE: Flag \"NUMITERATIONS\" is ");
 	checkFlag(flag, NUM) == true ? printf("on\n") : printf("off\n");
-	printf("VERBOSE: Value for flag \"NUMITERATIONS\" is %ld\n", n);
+	printf("VERBOSE: Value for flag \"NUMITERATIONS\" is %lld\n", n);
 	printf("VERBOSE: Flag \"MONTECARLO\" is ");
 	checkFlag(flag, MONTECARLO) == true ? printf("on\n") : printf("off\n");
+	printf("VERBOSE: Flag \"MONTECARLO_RADIUS\" is ");
+	checkFlag(flag, MONTECARLO_RADIUS) == true ? printf("on\n") : printf("off\n");
+	printf("VERBOSE: Value for flag \"MONTECARLO_RADIUS\" is %lld\n", r);
 }
